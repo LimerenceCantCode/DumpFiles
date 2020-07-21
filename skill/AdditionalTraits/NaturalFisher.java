f7cdf9b44f4ad5bca4b3e90c7bce31f355e1a36e
LimerenceCantCode <67572929+LimerenceCantCode@users.noreply.github.com>
2020-07-21 14:37:40 -0700

    Update NaturalFisher.java

--- skill/AdditionalTraits/NaturalFisher.java
+++ skill/AdditionalTraits/NaturalFisher.java
@@ -1,23 +1,11 @@
 package limerence.AdditionalTraits.skill.AdditionalTraits;
 
-import java.lang.reflect.Field;
-
 import codersafterdark.reskillable.api.unlockable.Trait;
 import limerence.AdditionalTraits.api.data.MessageCastLine;
 import limerence.AdditionalTraits.api.data.PacketHandler;
-import limerence.AdditionalTraits.api.unlockable.EventInterface;
-import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.projectile.EntityFishHook;
-import net.minecraft.init.Items;
-import net.minecraft.init.SoundEvents;
-import net.minecraft.item.ItemStack;
-import net.minecraft.stats.StatList;
-import net.minecraft.util.EnumHand;
 import net.minecraft.util.ResourceLocation;
-import net.minecraft.util.SoundCategory;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.RayTraceResult;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent.RightClickEmpty;
 import net.minecraftforge.fml.common.FMLCommonHandler;
@@ -41,8 +29,11 @@ public class TraitNaturalFisher extends Trait {
 	@SideOnly(Side.CLIENT)
 	public void onEmptyClick(RightClickEmpty event) {
 	EntityPlayer player = event.getEntityPlayer();
+
 	if (player.isSneaking() && player.getHeldItemMainhand().isEmpty()) {
+		PacketHandler.instance.sendToServer(new MessageCastLine());
 		CastLine(player, true);
+
 		}
 	}
 	
@@ -62,66 +53,73 @@ public class TraitNaturalFisher extends Trait {
 	 
 	 
 	 
+	//	PacketHandler.instance.sendToServer(new MessageCastLine());			
 	 
-	 
+		
+	 //, player.posX, player.posY, player.posZ
+
+	 //int lvt_5_1_ = fishEntity.handleHookRetraction();
+		
+	//, x, y, z
+	
+	//public ActionResult<ItemStack> onClickEmpty(World world, EntityPlayer player, EnumHand hand){
+		
+		//ItemStack DefaultRod = new ItemStack(Items.FISHING_ROD);
+		
+		//return new ActionResult(EnumActionResult.SUCCESS, Items.FISHING_ROD);}
 	
 	
-	public static void CastLine(final EntityPlayer player, final boolean isClient){
-		
-		if (player.fishEntity != null) {player.fishEntity.handleHookRetraction();}
-		else {
-		
-		EnumHand hand = player.getActiveHand();
-
-		//boolean isMain = player.getHeldItemMainhand().getItem() == null;
-		
-		RayTraceResult rt = player.rayTrace(3, 2);
-		BlockPos pos = rt.getBlockPos();
-		BlockPos offPos = new BlockPos(pos);
-
-		if (player.world.isAirBlock(offPos)) {System.out.println("Success at " +offPos);}
-
-		if (isClient == true) {System.out.println("This is a client event");
-		if (player.fishEntity != null) {player.fishEntity.handleHookRetraction();}
-		else {
-		
-		PacketHandler.instance.sendToServer(new MessageCastLine());
-		EntityFishHook MakeShiftHook = new EntityFishHook(player.world, player);
-		player.world.spawnEntity(MakeShiftHook);
-		
-		}
-		}
-		else {System.out.println("This is a server event");
-			
-		
-			if (player.fishEntity != null) {player.swingArm(hand);
-			player.fishEntity.handleHookRetraction();
-			player.world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_BOBBER_RETRIEVE, SoundCategory.NEUTRAL, 1.0F, 0.4F / (player.getRNG().nextFloat() * 0.4F + 0.8F));
-			}
-			else {
-		
-			
-				ItemStack DefaultFishingRod = new ItemStack(Items.FISHING_ROD);
-
-				EntityFishHook MakeShiftHook = new EntityFishHook(player.world, player, player.posX, player.posY, player.posZ);
-				
-				int SpeedBonus = EnchantmentHelper.getFishingSpeedBonus(DefaultFishingRod);
-				int LuckBonus = EnchantmentHelper.getFishingLuckBonus(DefaultFishingRod);
-				
-				if (SpeedBonus > 0) {MakeShiftHook.setLureSpeed(SpeedBonus);}
-				if (LuckBonus > 0) {MakeShiftHook.setLuck(LuckBonus);}
-
-				
-				player.world.spawnEntity(MakeShiftHook);
-
-				
-				//MakeShiftHook.handleHookRetraction();
-
-				player.world.playSound(null, player.posX, player.posY, player.posZ, SoundEvents.ENTITY_BOBBER_THROW, SoundCategory.NEUTRAL, 0.5F, 0.4F / (player.getRNG().nextFloat() * 0.4F + 0.8F));
-				player.swingArm(hand);
-		
-			}
-			}	
+	public static void CastLine(EntityPlayer player, boolean isClient){
+	if(isClient == false) {
+		if(player.fishEntity == null) {
+			EntityFishHook MakeShiftHook = new EntityFishHook(player.world, player);
+			player.world.spawnEntity(MakeShiftHook);
 		}
 	}
+	
+	else {
+		if(player.fishEntity == null) {
+			EntityFishHook MakeShiftHook = new EntityFishHook(player.world, player);	
+			player.world.spawnEntity(MakeShiftHook); 
+	
+		}
+	}
+		
+			
+	System.out.println("This is Clientside: " +isClient);	
+		
+		
+
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
+
+				
+	
+
+				
+				
+				
+
+
+
+
+
+	//&& player.isElytraFlying() || !player.onGround || player.capabilities.isFlying
+	
+
+
